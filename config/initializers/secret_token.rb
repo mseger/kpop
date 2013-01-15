3a1cd3f8ebef10633ccdd1b2befcfbf646f45da6
mseger <margaret-ann.seger@students.olin.edu>
2013-01-15 14:04:28 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kpop::Application.config.secret_token = '4e27ea32b3a2cba2747bec7af2b9ee87c920933a8dfa8a6cf84d92c35f7cf11147f206ca9c70f1feb8a75cbea32a3c8e7a7d3f70001749a40641566b9f25393a'
