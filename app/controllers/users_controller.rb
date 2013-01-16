168146550e4a8f92f907cec254986be278ecb00e
Geoff Pleiss <gpleiss@gmail.com>
2013-01-15 22:09:51 -0800

    Cleaned up scaffolded users controller by getting rid of json responses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,13 @@
 class UsersController < ApplicationController
   # GET /users
-  # GET /users.json
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    redirect_to users_url
   end
 end
