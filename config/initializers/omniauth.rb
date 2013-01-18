8ce1a3100b758c2151e43355f41d325ea5ef28c9
Geoff Pleiss <gpleiss@gmail.com>
2013-01-17 21:55:44 -0800

    Moved fb app credentials to config file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
+FACEBOOK_KEYS = YAML.load_file("#{Rails.root}/config/facebook_keys.yml")[Rails.env]
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  provider :facebook, FACEBOOK_KEYS['app_id'], FACEBOOK_KEYS['secret']
 end
 
