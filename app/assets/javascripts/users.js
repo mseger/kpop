a1df1dd22966c712cefe75bd2ab7ecc453e11148
mseger <margaret-ann.seger@students.olin.edu>
2013-01-17 20:10:36 -0800

    Added radio buttons for loudness and house-party prefernces

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,24 @@
-$('.btn.find-roommates').live("click", function (e) {
+$(".btn.find-roommates").live("click", function (e) {
   $.ajax({
     url: "/generate_matches",
     type: "POST"
   });
+});
+
+// loudness radio buttons
+$(".btn.quiet").live("click", function (e) {
+	$("input#user_loudness").val("f");
+});
+
+$(".btn.loud").live("click", function (e){
+	$("input#user_loudness").val("t");
+});
+
+// house-party radio buttons
+$(".btn.okay").live("click", function (e) {
+	$("input#user_house_parties").val("t");
+});
+
+$(".btn.not-okay").live("click", function (e){
+	$("input#user_house_parties").val("f");
 });
